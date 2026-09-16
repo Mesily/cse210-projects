46399205c42d58bf730b607621de96a187d34bac
Mesily <animercy210@gmail.com>
2026-09-16 07:40:13 -0700

    Resume created

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "Reinforced Global";
+        job1._jobTitle = "Secretary";
+        job1._startYear = 2021;
+        job1._endYear = 2026;
+
+      
+
+        Job job2 = new Job();
+        job2._company = "Devine Impact Profile.";
+        job2._jobTitle = "Research Assistant";
+        job2._startYear = 2017;
+        job2._endYear = 2021;
+
+     
+
+        Resume resume = new Resume();
+    resume._name = "Mercy Ani";
+    resume._jobs.Add(job1);
+    resume._jobs.Add(job2);
+    resume.Display();
     }
+    
+
 }
