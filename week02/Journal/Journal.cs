1583ff08d7e94d523a014ebff15c08d574ae129b
Mesily <animercy210@gmail.com>
2026-09-19 09:56:59 -0700

    created template for each classes of the journal project

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+public void DisplayAll()
+    {
+    
+    }
+
+    public void SaveToFile(string file)
+    {
+        
+    }
+    public void LoadFromFile(string file)
+    {
+        
+    }
+}
