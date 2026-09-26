9137eb4b9f9585d13b6e184ccc79e814f7741187
Mesily <animercy210@gmail.com>
2026-09-26 11:24:58 -0700

    created four files including a text file for scripture memorizer project

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        List<Scripture> scriptures = LoadScriptures("scriptures.txt");
+
+        Random random = new Random();
+
+        int number = random.Next(scriptures.Count);
+        Scripture scripture = scriptures[number];
+
+        while (!scripture.AllWordsHidden())
+        {
+            Console.Clear();
+
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to hide a word or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords();
+        }
     }
+
+    static List<Scripture> LoadScriptures(string fileName)
+    {
+        List<Scripture> scriptures = new List<Scripture>();
+
+        string[] lines = File.ReadAllLines(fileName);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+   
+            string referencePart = parts[0];
+            string textPart = parts[1];
+
+            string[] referenceParts = referencePart.Split(' ');
+
+            string book = referenceParts[0];
+
+            string[] chapterVerse = referenceParts[1].Split(':');
+
+            int chapter = int.Parse(chapterVerse[0]);
+            int verse = int.Parse(chapterVerse[1]);
+
+            Reference reference = new Reference(book, chapter, verse);
+
+            Scripture scripture = new Scripture(reference, textPart);
+
+            scriptures.Add(scripture);
+        }
+
+        return scriptures;
+    }
+
+    
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,64 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords()
+    {
+        Random random = new Random();
+
+        List<Word> visibleWords = new List<Word>();
+
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                visibleWords.Add(word);
+            }
+        }
+
+        if (visibleWords.Count > 0)
+        {
+            int number = random.Next(visibleWords.Count);
+            visibleWords[number].Hide();
+        }
+    }
+
+    public bool AllWordsHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + "\n";
+
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+
+        return result;
+    }
+}
